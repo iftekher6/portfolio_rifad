de85f6f4a8532aeaf0aee910847b47194af90799
iftekher ahmed <139613310+iftekher6@users.noreply.github.com>
2024-10-07 18:24:26 +0600

    reconsidering

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <>
         {/* <Hero/> */}
         <HeroShowReel/>
-        <Partners/>
+        {/* <Partners/> */}
         <Portfolio/>
       
         <CallToAction/>
